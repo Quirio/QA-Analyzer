4ad9733166deee8b622f7b14871af718817db87a
Alejandro Hernández Hernández <Quirio123@gmail.com>
2016-09-27 13:38:53 +0200

    Changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,7 @@
         <title>Lucía es una gandula</title>
 
         <!-- Fonts -->
+
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         <link href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.4/semantic.min.css" rel="stylesheet" type="text/css">		
     </head>
@@ -199,7 +200,7 @@
 				</div>
 			</div>			
 		</div>
-
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js" type="text/javascript"></script> 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.4/semantic.min.js" type="text/javascript"></script> 
     </body>
 </html>
