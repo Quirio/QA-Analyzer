110dd71c5d43dc80db0c039582e93741e4663d30
Alejandro Hernández Hernández <Quirio123@gmail.com>
2016-09-30 12:14:18 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,13 @@
 | to using a Closure or controller method. Build something great!
 |
 */
-
-/*Route::get('/', function () {
+*Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/', function () {
     return view('home');
-});*/
+});
 
 
 //Auth::routes();
